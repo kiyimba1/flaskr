c59533f8270f9dfac9f6d0c34750723aecf56c11
Edwin Kiyimba <edwineddie54@gmail.com>
2021-01-22 11:50:52 +0300

    Rich-Text Posts with Markdown and Flask-PageDown

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Regexp, ValidationError, Email
+from flask_pagedown.fields import PageDownField
 
 from app.models import Role, User
 
@@ -43,5 +44,5 @@ class EditProfileAdminForm(FlaskForm):
             raise ValidationError('Username already in use')
 
 class PostForm(FlaskForm):
-    body = TextAreaField("What's on your mind?", validators=[DataRequired()])
+    body = PageDownField("What's on your mind?", validators=[DataRequired()])
     submit = SubmitField('Submit')
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 import hashlib
 from datetime import datetime
+from markdown import markdown
+import bleach
 
 from flask import current_app, request
 from flask_sqlalchemy import SQLAlchemy
@@ -181,4 +183,14 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    aurhor_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol','pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkifier(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True
+        ))
+    db.event.listen(Post.body, 'set', Post.on_changed_body)
